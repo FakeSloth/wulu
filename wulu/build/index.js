c58665d8a13f5a9dadaee95bc1541bc8b599d0d5
Phil <birkal@outlook.com>
2015-03-25 20:10:11 -0700

    Update include files

---
+++ wulu/build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
+
+var Mongo = _interopRequire(require("./mongo"));
+
+module.exports = {
+  Mongo: Mongo
+};
---
+++ wulu/build/mongo.js
@@ -0,0 +1,93 @@
+"use strict";
+
+var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
+
+var mongoose = _interopRequire(require("mongoose"));
+
+module.exports = {
+  connect_database: connect_database,
+  importUsergroups: importUsergroups,
+  exportUsergroups: exportUsergroups
+};
+
+var userSchema = new mongoose.Schema({
+  name: { type: String, lowercase: true },
+  group: String
+});
+
+var User = mongoose.model("user", userSchema);
+
+/**
+ * Connect to MongoDB Database.
+ */
+
+function connect_database() {
+  var url = process.env.MONGODB || "mongodb://localhost:27017/ps";
+  mongoose.connect(url);
+  mongoose.connection.on("error", function () {
+    return console.error("MongoDB Connection Error. Make sure MongoDB is running.");
+  });
+};
+
+/**
+ * Get usergroups from MongoDB.
+ *
+ * @param {Object} usergroups
+ * @param {Object} Config
+ */
+
+function importUsergroups(usergroups, Config) {
+  User.find({}, function (err, users) {
+    if (err) return;
+    users.forEach(function (user) {
+      return usergroups[user.name] = (user.group || Config.groupsranking[0]) + user.name;
+    });
+  });
+};
+
+/**
+ * Save usergroups to MongoDB.
+ *
+ * @param {Object} usergroups
+ */
+
+function exportUsergroups(usergroups) {
+  var users = [];
+  for (var i in usergroups) {
+    users.push({
+      name: usergroups[i].substr(1).replace(/,/g, ""),
+      group: usergroups[i].substr(0, 1)
+    });
+  }
+  users.forEach(function (user) {
+    User.findOne({ name: user.name.toLowerCase() }, function (err, userModel) {
+      if (err) return;
+      if (!userModel) {
+        user = new User({
+          name: user.name,
+          group: user.group
+        });
+        return user.save();
+      }
+      userModel.group = user.group;
+      userModel.save();
+    });
+  });
+
+  // Demote to regular user
+  users = users.map(function (user) {
+    return user.name.toLowerCase();
+  });
+  User.find({}, function (err, usersModel) {
+    if (err) return;
+    usersModel.forEach(function (user) {
+      if (users.indexOf(user.name) < 0) {
+        User.findOne({ name: user.name }, function (err, user) {
+          if (err) return;
+          user.group = "";
+          user.save();
+        });
+      }
+    });
+  });
+};
