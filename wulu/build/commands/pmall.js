1968fcdc3e1bb6bcc825186048d5fec0923e4e87
Phil <birkal@outlook.com>
2015-04-06 16:30:11 -0700

    Add /pmall command

---
+++ wulu/build/commands/pmall.js
@@ -0,0 +1,31 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+exports['default'] = pmall;
+
+/**
+ * Private messages all users on the server.
+ */
+
+function pmall() {
+  var name = arguments[0] === undefined ? '~Server PM [Do not reply]' : arguments[0];
+
+  var commands = {
+    masspm: 'pmall',
+    pmall: function pmall(target, room, user) {
+      if (!this.can('pmall')) {
+        return false;
+      }if (!target) {
+        return this.sendReply('/pmall [message]');
+      }for (var i in Users.users) {
+        var message = '|pm|' + name + '|' + Users.users[i].getIdentity() + '|' + target;
+        Users.users[i].send(message);
+      }
+    }
+  };
+
+  Object.merge(CommandParser.commands, commands);
+}
+module.exports = exports['default'];
---
+++ wulu/src/commands/pmall.js
@@ -0,0 +1,22 @@
+export default pmall;
+
+/**
+ * Private messages all users on the server.
+ */
+
+function pmall(name='~Server PM [Do not reply]') {
+  let commands = {
+    masspm: 'pmall',
+    pmall(target, room, user) {
+      if (!this.can('pmall')) return false;
+      if (!target) return this.sendReply('/pmall [message]');
+
+      for (let i in Users.users) {
+        let message = `|pm|${name}|${Users.users[i].getIdentity()}|${target}`;
+        Users.users[i].send(message);
+      }
+    }
+  };
+
+  Object.merge(CommandParser.commands, commands);
+}
